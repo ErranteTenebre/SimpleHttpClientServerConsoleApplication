d0dadddef2f409ca67efeef7d23d477b9df60b14
ErranteTenebre <mrm.aimdar@mail.ru>
2024-11-30 00:53:40 +0300

    initialize

---
+++ HttpClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace HttpClient2
+{
+    internal class Program
+    {
+        private const string _serverIp = "127.0.0.1";
+        private const int _serverPort = 8080;
+        private static readonly string requestURI = $"http://{_serverIp}:{_serverPort}/";
+
+        private const string payload = "Hi world, I'm Aidar";
+        static async Task Main(string[] args)
+        {
+            HttpClient httpClient = new HttpClient();
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestURI);
+
+            request.Content = new StringContent(payload, Encoding.UTF8);
+
+            HttpResponseMessage response = await httpClient.SendAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseText = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(responseText);
+            }
+            else
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+        }
+    }
+}
---
+++ HttpServer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    private const string _serverIp = "127.0.0.1";
+    private const int _serverPort = 8080;
+
+    private static string responseString = "<html><div>Привет</div></html>";
+    static async Task Main(string[] args)
+    {
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add($"http://{_serverIp}:{_serverPort}/");
+
+        listener.Start();
+
+        Console.WriteLine($"Сервер слушает по ip {_serverIp} на порту {_serverPort}");
+
+        while (true)
+        {
+            HttpListenerContext httpContext = await listener.GetContextAsync();
+
+            HttpListenerRequest request = httpContext.Request;
+
+            HttpListenerResponse response = httpContext.Response;
+
+            if (request.Headers["Content-Length"] == null)
+            {
+                response.StatusCode = 411;
+                response.OutputStream.Close();
+            }
+
+            ConfigureHttpResponseHeaders(response);
+
+            byte[] responseBytes = Encoding.UTF8.GetBytes(responseString);
+
+            try
+            {
+                await response.OutputStream.WriteAsync(responseBytes);
+                Console.WriteLine("Ответ отправлен");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка при отправке ответа - " + ex.ToString());
+            }
+            finally
+            {
+                response.OutputStream.Close();
+            }
+        }
+    }
+
+    static void ConfigureHttpResponseHeaders(HttpListenerResponse response)
+    {
+        response.Headers.Add("Access-Control-Allow-Origin", "*");
+        response.Headers.Add("Access-Control-Allow-Methods", "GET");
+        response.Headers.Add("Access-Control-Allow-Headers", "*");
+
+        response.Headers.Add("Content-type", "text/html");
+    }
+}
